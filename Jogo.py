08c671b03e42145462c813c4f203b2137c4c27ed
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2020-01-22 16:13:07 -0300

    Metodo de distribuição de cartas

--- Jogo.py
+++ Jogo.py
@@ -1,3 +1,5 @@
+import random
+
 class Jogador:
     
     def __init__(self, nome):
@@ -40,7 +42,16 @@ class Jogo:
                 else:
                     self.stack.append(j)
         arquivo.close()
-        print(self.stack)
+    
+    def deckGen(self):
+        auxJogador = self.inicioFila
+        for i in range(self.nJogadores):
+            for j in range(7):
+                x = random.choice(self.stack)
+                auxJogador.deck.append(x)
+                self.stack.remove(x)
+                auxJogador = auxJogador.getProxJogador()
+        self.imprimirFilaJogadores()
 
     def filaJogadores(self, numPlayers):
         for i in range(numPlayers):
@@ -61,27 +72,26 @@ class Jogo:
                     self.fimFila = novoJogador
                     self.fimFila.setProxJogador(self.inicioFila)
                     self.inicioFila.setAnteriorJogador(self.fimFila)
-        self.imprimir()
 
-    def imprimir(self):
+    def imprimirFilaJogadores(self):
         noAux = self.inicioFila
         noAux2 = self.fimFila
         saida = ''
         saida2 = ''
         for i in range(self.nJogadores):
             saida += str(noAux.getNome()) + " / "
+            print(noAux.nome)
+            print(noAux.deck)
             noAux = noAux.getProxJogador()
-        for i in range(self.nJogadores):
-            saida2 += str(noAux2.getNome()) + " / "
-            noAux2 = noAux2.getAnteriorJogador()
-        print(saida)
-        print()
-        print(saida2)
+        #for i in range(self.nJogadores):
+        #    saida2 += str(noAux2.getNome()) + " / "
+        #    noAux2 = noAux2.getAnteriorJogador()
 
 
     def partida(self):
         self.filaJogadores(self.nJogadores)
         self.stackGen()
+        self.deckGen()
 
 
 njogadores = int(input("Quantos jogadores irao jogar ?\nObs: Min: 2 jogadores.\nMax: 10 jogadores.\n"))
